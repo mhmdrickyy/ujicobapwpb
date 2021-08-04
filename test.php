874def8d4c155e319b5ae69a3eefd2ecd868c409
Muhammad Ricky <88218741+mhmdrickyy@users.noreply.github.com>
2021-08-04 19:13:09 +0700

    Add files via upload

---
+++ test.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title><?php echo "Belajar PHP" ?></title>
+    </head>
+    <body>
+        <?php
+	
+            echo "Nama : Muhammad Ricky Adriansyah<br> ";
+            echo "<p>Kelas : XI RPL II<p>";
+			echo "<p>Sekolah : SMK Mahardhika";
+			echo "<p>Cita Cita : Game Developer"
+        ?>
+    </body>
+</html>
